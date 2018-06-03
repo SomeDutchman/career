e9f7dd85264c383eccbfbbe58a03d29c456a1692
SomeDutchman <39811793+SomeDutchman@users.noreply.github.com>
2018-06-03 12:50:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
-    echo 'Test';
+	session_start();
+	// Connect to the database
+	include "php/connect.php";
 ?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<?php include "php/include/head.php"; ?>
+	</head>
+	<body>
+		<!-- Header -->
+		<section id="header">
+		</section>
+		<!-- Page -->
+		<section id="page">
+		</section>
+	</body>
+	<!-- Scripts -->
+	<?php include "php/include/scripts.php"; ?>
+</html>
