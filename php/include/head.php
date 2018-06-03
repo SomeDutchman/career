d9bdf096fc00a35a9a02d324c2c17a0411f861b5
SomeDutchman <39811793+SomeDutchman@users.noreply.github.com>
2018-06-03 12:55:28 +0200

    Create head.php

---
+++ php/include/head.php
@@ -0,0 +1,12 @@
+<!-- Meta -->
+<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<!-- Icon and title -->
+<title>CPT | Career Platform Tilburg</title>
+<!-- Font Awesome (icons) -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" integrity="sha384-3AB7yXWz4OeoZcPbieVW64vVXEwADiYyAEhwilzWsLw+9FgqpyjjStpPnpBO8o8S" crossorigin="onymous">
+<!-- jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<!-- Standard stylesheets -->
+<link rel="stylesheet" href="css/fonts.css">
+<link rel="stylesheet" href="css/style.css">
